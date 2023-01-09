39faa46288451123b84385729b4ad6f8eb791a81
yardenco <yarden.cohen5521@gmail.com>
2023-01-09 18:52:37 +0200

    need to validate all fields in login/register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('register', { title: 'Express' });
 });
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+
+router.post('/login', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+
+
+});
+
+router.get('/password', function(req, res, next) {
+  res.render('password', { title: 'Express' });
+});
+
+router.post('/password', function(req, res, next) {
+  res.render('password', { title: 'Express' });
+});
+
 
 module.exports = router;
