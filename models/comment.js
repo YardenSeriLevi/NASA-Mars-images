46d0fa2b4648401dadee08d109242ebf10d7fabc
yardenco <yarden.cohen5521@gmail.com>
2023-01-23 14:54:13 +0200

    need to fix commentControler line28 then create scrole anmation, and keep moving

---
+++ models/comment.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Comment.belongsTo(sequelize.models.Contact, {
+        foreignKey: 'user_id'
+      })
+    }
+  }
+
+  Comment.init({
+    user_id: DataTypes.INTEGER,
+    identify: DataTypes.STRING,
+    comment: DataTypes.STRING,
+    date: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
+
+
